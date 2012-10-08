bdbb309785a77fe4d57b2a4ff760c2f5d2733e30
shinriyo <shinriyo@gmail.com>
2012-10-08 20:25:47 +0900

    Initial Commit

---
+++ papwa/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  papwaAppController.h
+//  papwa
+//
+//  Created by shinriyo on 10/8/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ papwa/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  papwa
+//
+//  Created by shinriyo on 10/8/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
